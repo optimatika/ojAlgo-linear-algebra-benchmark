c260902d3c62540588ec7034b1220706eb36ce3f
apete <apete@optimatika.se>
2017-03-15 22:47:36 +0100

    10: Tridiagonal
    Task-Url: http://bugzilla.optimatika.se/show_bug.cgi?id=10

--- src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
+++ src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
@@ -31,52 +31,52 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.runner.RunnerException;
 
 /**
- * Mac Pro 2015-07-01
+ * Mac Pro 2017-03-15
  *
  * <pre>
-# Run complete. Total time: 00:13:36
+# Run complete. Total time: 00:23:15
 
-Benchmark               (dim)  (library)   Mode  Cnt         Score         Error    Units
-DecomposeEigen.execute      2       EJML  thrpt    3  88419411,622 ± 2632421,642  ops/min
-DecomposeEigen.execute      2        MTJ  thrpt    3   7960120,758 ±  131808,738  ops/min
-DecomposeEigen.execute      2     ojAlgo  thrpt    3  90079803,092 ± 1241935,865  ops/min
-DecomposeEigen.execute      3       EJML  thrpt    3  30763741,324 ± 1332825,150  ops/min
-DecomposeEigen.execute      3        MTJ  thrpt    3   3782723,778 ±  278004,057  ops/min
-DecomposeEigen.execute      3     ojAlgo  thrpt    3  36140444,133 ±  405368,262  ops/min
-DecomposeEigen.execute      4       EJML  thrpt    3  16099265,219 ±  211426,044  ops/min
-DecomposeEigen.execute      4        MTJ  thrpt    3   3081823,874 ±   78593,344  ops/min
-DecomposeEigen.execute      4     ojAlgo  thrpt    3  25876373,025 ± 3492652,031  ops/min
-DecomposeEigen.execute      5       EJML  thrpt    3  12344613,990 ±  389596,033  ops/min
-DecomposeEigen.execute      5        MTJ  thrpt    3   2553529,480 ±  107826,612  ops/min
-DecomposeEigen.execute      5     ojAlgo  thrpt    3  16593123,088 ±  426553,403  ops/min
-DecomposeEigen.execute     10       EJML  thrpt    3   2838306,797 ±  192581,259  ops/min
-DecomposeEigen.execute     10        MTJ  thrpt    3   1257630,710 ±   12740,192  ops/min
-DecomposeEigen.execute     10     ojAlgo  thrpt    3   4548650,123 ±  127705,006  ops/min
-DecomposeEigen.execute     20       EJML  thrpt    3    589257,561 ±   11316,301  ops/min
-DecomposeEigen.execute     20        MTJ  thrpt    3    537911,085 ±   19071,275  ops/min
-DecomposeEigen.execute     20     ojAlgo  thrpt    3   1093043,013 ±   57693,924  ops/min
-DecomposeEigen.execute     50       EJML  thrpt    3     71166,939 ±    2520,196  ops/min
-DecomposeEigen.execute     50        MTJ  thrpt    3     87017,786 ±    2774,765  ops/min
-DecomposeEigen.execute     50     ojAlgo  thrpt    3    121113,268 ±    2833,273  ops/min
-DecomposeEigen.execute    100       EJML  thrpt    3     12388,172 ±     623,489  ops/min
-DecomposeEigen.execute    100        MTJ  thrpt    3     15264,238 ±     974,360  ops/min
-DecomposeEigen.execute    100     ojAlgo  thrpt    3     20448,991 ±     320,693  ops/min
-DecomposeEigen.execute    200       EJML  thrpt    3      1838,544 ±      41,112  ops/min
-DecomposeEigen.execute    200        MTJ  thrpt    3      4110,957 ±     227,798  ops/min
-DecomposeEigen.execute    200     ojAlgo  thrpt    3      3091,946 ±       6,959  ops/min
-DecomposeEigen.execute    500       EJML  thrpt    3       131,009 ±      10,196  ops/min
-DecomposeEigen.execute    500        MTJ  thrpt    3       582,979 ±      43,421  ops/min
-DecomposeEigen.execute    500     ojAlgo  thrpt    3       211,055 ±       2,400  ops/min
-DecomposeEigen.execute   1000       EJML  thrpt    3        15,013 ±       1,056  ops/min
-DecomposeEigen.execute   1000        MTJ  thrpt    3       107,251 ±       4,268  ops/min
-DecomposeEigen.execute   1000     ojAlgo  thrpt    3        26,805 ±       9,113  ops/min
-DecomposeEigen.execute   2000       EJML  thrpt    3         1,734 ±       0,041  ops/min
-DecomposeEigen.execute   2000        MTJ  thrpt    3        16,002 ±       1,247  ops/min
-DecomposeEigen.execute   2000     ojAlgo  thrpt    3         2,692 ±       0,109  ops/min
+Benchmark               (dim)  (library)   Mode  Cnt        Score       Error  Units
+DecomposeEigen.execute      2       EJML  thrpt    5  1588948.697 ± 10522.036  ops/s
+DecomposeEigen.execute      2        MTJ  thrpt    5   136739.957 ±  1588.252  ops/s
+DecomposeEigen.execute      2     ojAlgo  thrpt    5  1390676.198 ±  5230.988  ops/s
+DecomposeEigen.execute      3       EJML  thrpt    5   586254.824 ±  2782.997  ops/s
+DecomposeEigen.execute      3        MTJ  thrpt    5    61974.174 ±  2109.253  ops/s
+DecomposeEigen.execute      3     ojAlgo  thrpt    5   691992.388 ±  3039.329  ops/s
+DecomposeEigen.execute      4       EJML  thrpt    5   328198.423 ±  2484.400  ops/s
+DecomposeEigen.execute      4        MTJ  thrpt    5    49345.305 ±   362.092  ops/s
+DecomposeEigen.execute      4     ojAlgo  thrpt    5   416739.575 ±  1953.413  ops/s
+DecomposeEigen.execute      5       EJML  thrpt    5   225819.643 ±  1029.626  ops/s
+DecomposeEigen.execute      5        MTJ  thrpt    5    40075.590 ±  1050.354  ops/s
+DecomposeEigen.execute      5     ojAlgo  thrpt    5   312181.215 ±  1783.941  ops/s
+DecomposeEigen.execute     10       EJML  thrpt    5    53854.242 ±   173.035  ops/s
+DecomposeEigen.execute     10        MTJ  thrpt    5    18692.964 ±  1216.545  ops/s
+DecomposeEigen.execute     10     ojAlgo  thrpt    5    77584.149 ±   876.953  ops/s
+DecomposeEigen.execute     20       EJML  thrpt    5    10799.539 ±    42.450  ops/s
+DecomposeEigen.execute     20        MTJ  thrpt    5     7928.053 ±    21.067  ops/s
+DecomposeEigen.execute     20     ojAlgo  thrpt    5    20241.613 ±    37.410  ops/s
+DecomposeEigen.execute     50       EJML  thrpt    5     1242.983 ±    25.181  ops/s
+DecomposeEigen.execute     50        MTJ  thrpt    5     1524.953 ±    18.243  ops/s
+DecomposeEigen.execute     50     ojAlgo  thrpt    5     1996.984 ±     5.000  ops/s
+DecomposeEigen.execute    100       EJML  thrpt    5      207.909 ±     0.461  ops/s
+DecomposeEigen.execute    100        MTJ  thrpt    5      310.073 ±     3.670  ops/s
+DecomposeEigen.execute    100     ojAlgo  thrpt    5      340.594 ±     1.125  ops/s
+DecomposeEigen.execute    200       EJML  thrpt    5       31.712 ±     0.196  ops/s
+DecomposeEigen.execute    200        MTJ  thrpt    5       75.214 ±     1.558  ops/s
+DecomposeEigen.execute    200     ojAlgo  thrpt    5       51.367 ±     1.074  ops/s
+DecomposeEigen.execute    500       EJML  thrpt    5        2.293 ±     0.014  ops/s
+DecomposeEigen.execute    500        MTJ  thrpt    5       10.834 ±     0.299  ops/s
+DecomposeEigen.execute    500     ojAlgo  thrpt    5        3.566 ±     0.074  ops/s
+DecomposeEigen.execute   1000       EJML  thrpt    5        0.256 ±     0.001  ops/s
+DecomposeEigen.execute   1000        MTJ  thrpt    5        2.095 ±     0.106  ops/s
+DecomposeEigen.execute   1000     ojAlgo  thrpt    5        0.452 ±     0.022  ops/s
+DecomposeEigen.execute   2000       EJML  thrpt    5        0.030 ±     0.001  ops/s
+DecomposeEigen.execute   2000        MTJ  thrpt    5        0.305 ±     0.008  ops/s
+DecomposeEigen.execute   2000     ojAlgo  thrpt    5        0.046 ±     0.001  ops/s
  * </pre>
  *
  * MacBook Air: 2015-06-16
- *
+ * 
  * <pre>
  * # Run complete. Total time: 01:41:52
  *
@@ -126,42 +126,41 @@ DecomposeEigen.execute   1000     ojAlgo  thrpt    3        25,599 ±        2,0
 @State(Scope.Benchmark)
 public class DecomposeEigen extends LinearAlgebraBenchmark {
 
-	public static void main(final String[] args) throws RunnerException {
-		LinearAlgebraBenchmark.run(DecomposeEigen.class);
-	}
+    public static void main(final String[] args) throws RunnerException {
+        LinearAlgebraBenchmark.run(DecomposeEigen.class);
+    }
 
-	@Param({ "2", "3", "4", "5", "10", "20", "50", "100", "200", "500", "1000",
-			"2000"/* , "5000", "10000" */ })
-	public int dim;
-	@Param({ "EJML", "MTJ", "ojAlgo" })
-	public String library;
+    @Param({ "2", "3", "4", "5", "10", "20", "50", "100", "200", "500", "1000", "2000"/* , "5000", "10000" */ })
+    public int dim;
+    @Param({ "EJML", "MTJ", "ojAlgo" })
+    public String library;
 
-	Object matrix;
+    Object matrix;
 
-	private BenchmarkContestant<?>.EigenDecomposer myDecomposer;
+    private BenchmarkContestant<?>.EigenDecomposer myDecomposer;
 
-	@Override
-	@Benchmark
-	public Object execute() {
-		return myDecomposer.decompose(matrix);
-	}
+    @Override
+    @Benchmark
+    public Object execute() {
+        return myDecomposer.decompose(matrix);
+    }
 
-	@Setup
-	public void setup() {
+    @Setup
+    public void setup() {
 
-		contestant = BenchmarkContestant.CONTESTANTS.get(library);
+        contestant = BenchmarkContestant.CONTESTANTS.get(library);
 
-		matrix = contestant.convert(this.makeSPD(dim));
+        matrix = contestant.convert(this.makeSPD(dim));
 
-		myDecomposer = contestant.getEigenDecomposer();
-	}
+        myDecomposer = contestant.getEigenDecomposer();
+    }
 
-	@Override
-	@TearDown(Level.Iteration)
-	public void verify() throws BenchmarkRequirementsException {
+    @Override
+    @TearDown(Level.Iteration)
+    public void verify() throws BenchmarkRequirementsException {
 
-		this.verifyStateless(myDecomposer.getClass());
+        this.verifyStateless(myDecomposer.getClass());
 
-	}
+    }
 
 }
