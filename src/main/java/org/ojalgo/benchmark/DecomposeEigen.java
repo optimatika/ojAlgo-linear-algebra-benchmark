870ba7ca0c920242315c63e8bc4edfb63cc827c1
apete <apete@optimatika.se>
2017-03-27 23:45:19 +0200

    2017-03-27

--- src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
+++ src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
@@ -31,52 +31,60 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.runner.RunnerException;
 
 /**
- * Mac Pro 2017-03-15
+ * Mac Pro 2017-03-27
  *
  * <pre>
-# Run complete. Total time: 00:23:15
+# Run complete. Total time: 00:41:48
 
 Benchmark               (dim)  (library)   Mode  Cnt        Score       Error  Units
-DecomposeEigen.execute      2       EJML  thrpt    5  1588948.697 ± 10522.036  ops/s
-DecomposeEigen.execute      2        MTJ  thrpt    5   136739.957 ±  1588.252  ops/s
-DecomposeEigen.execute      2     ojAlgo  thrpt    5  1390676.198 ±  5230.988  ops/s
-DecomposeEigen.execute      3       EJML  thrpt    5   586254.824 ±  2782.997  ops/s
-DecomposeEigen.execute      3        MTJ  thrpt    5    61974.174 ±  2109.253  ops/s
-DecomposeEigen.execute      3     ojAlgo  thrpt    5   691992.388 ±  3039.329  ops/s
-DecomposeEigen.execute      4       EJML  thrpt    5   328198.423 ±  2484.400  ops/s
-DecomposeEigen.execute      4        MTJ  thrpt    5    49345.305 ±   362.092  ops/s
-DecomposeEigen.execute      4     ojAlgo  thrpt    5   416739.575 ±  1953.413  ops/s
-DecomposeEigen.execute      5       EJML  thrpt    5   225819.643 ±  1029.626  ops/s
-DecomposeEigen.execute      5        MTJ  thrpt    5    40075.590 ±  1050.354  ops/s
-DecomposeEigen.execute      5     ojAlgo  thrpt    5   312181.215 ±  1783.941  ops/s
-DecomposeEigen.execute     10       EJML  thrpt    5    53854.242 ±   173.035  ops/s
-DecomposeEigen.execute     10        MTJ  thrpt    5    18692.964 ±  1216.545  ops/s
-DecomposeEigen.execute     10     ojAlgo  thrpt    5    77584.149 ±   876.953  ops/s
-DecomposeEigen.execute     20       EJML  thrpt    5    10799.539 ±    42.450  ops/s
-DecomposeEigen.execute     20        MTJ  thrpt    5     7928.053 ±    21.067  ops/s
-DecomposeEigen.execute     20     ojAlgo  thrpt    5    20241.613 ±    37.410  ops/s
-DecomposeEigen.execute     50       EJML  thrpt    5     1242.983 ±    25.181  ops/s
-DecomposeEigen.execute     50        MTJ  thrpt    5     1524.953 ±    18.243  ops/s
-DecomposeEigen.execute     50     ojAlgo  thrpt    5     1996.984 ±     5.000  ops/s
-DecomposeEigen.execute    100       EJML  thrpt    5      207.909 ±     0.461  ops/s
-DecomposeEigen.execute    100        MTJ  thrpt    5      310.073 ±     3.670  ops/s
-DecomposeEigen.execute    100     ojAlgo  thrpt    5      340.594 ±     1.125  ops/s
-DecomposeEigen.execute    200       EJML  thrpt    5       31.712 ±     0.196  ops/s
-DecomposeEigen.execute    200        MTJ  thrpt    5       75.214 ±     1.558  ops/s
-DecomposeEigen.execute    200     ojAlgo  thrpt    5       51.367 ±     1.074  ops/s
-DecomposeEigen.execute    500       EJML  thrpt    5        2.293 ±     0.014  ops/s
-DecomposeEigen.execute    500        MTJ  thrpt    5       10.834 ±     0.299  ops/s
-DecomposeEigen.execute    500     ojAlgo  thrpt    5        3.566 ±     0.074  ops/s
-DecomposeEigen.execute   1000       EJML  thrpt    5        0.256 ±     0.001  ops/s
-DecomposeEigen.execute   1000        MTJ  thrpt    5        2.095 ±     0.106  ops/s
-DecomposeEigen.execute   1000     ojAlgo  thrpt    5        0.452 ±     0.022  ops/s
-DecomposeEigen.execute   2000       EJML  thrpt    5        0.030 ±     0.001  ops/s
-DecomposeEigen.execute   2000        MTJ  thrpt    5        0.305 ±     0.008  ops/s
-DecomposeEigen.execute   2000     ojAlgo  thrpt    5        0.046 ±     0.001  ops/s
+DecomposeEigen.execute      2        ACM  thrpt   15   536795.948 ± 22510.418  ops/s
+DecomposeEigen.execute      2       EJML  thrpt   15  1426954.650 ± 55599.347  ops/s
+DecomposeEigen.execute      2        MTJ  thrpt   15   129182.634 ±  4890.072  ops/s
+DecomposeEigen.execute      2     ojAlgo  thrpt   15  1838395.032 ± 56550.904  ops/s
+DecomposeEigen.execute      3        ACM  thrpt   15   339504.639 ± 17850.133  ops/s
+DecomposeEigen.execute      3       EJML  thrpt   15   564488.161 ± 15962.775  ops/s
+DecomposeEigen.execute      3        MTJ  thrpt   15    57882.905 ±   997.330  ops/s
+DecomposeEigen.execute      3     ojAlgo  thrpt   15   793904.265 ± 39630.311  ops/s
+DecomposeEigen.execute      4        ACM  thrpt   15   233153.670 ±  6469.204  ops/s
+DecomposeEigen.execute      4       EJML  thrpt   15   299188.415 ± 18598.584  ops/s
+DecomposeEigen.execute      4        MTJ  thrpt   15    45431.576 ±   624.977  ops/s
+DecomposeEigen.execute      4     ojAlgo  thrpt   15   501529.762 ± 24517.456  ops/s
+DecomposeEigen.execute      5        ACM  thrpt   15   164323.905 ±  6172.702  ops/s
+DecomposeEigen.execute      5       EJML  thrpt   15   213353.287 ±  7279.367  ops/s
+DecomposeEigen.execute      5        MTJ  thrpt   15    38215.573 ±  1646.812  ops/s
+DecomposeEigen.execute      5     ojAlgo  thrpt   15   314517.883 ± 20166.147  ops/s
+DecomposeEigen.execute     10        ACM  thrpt   15    43903.572 ±  1235.683  ops/s
+DecomposeEigen.execute     10       EJML  thrpt   15    51787.623 ±  2901.456  ops/s
+DecomposeEigen.execute     10        MTJ  thrpt   15    19437.337 ±   500.965  ops/s
+DecomposeEigen.execute     10     ojAlgo  thrpt   15    79890.729 ±  4897.018  ops/s
+DecomposeEigen.execute     20        ACM  thrpt   15     9248.306 ±    73.431  ops/s
+DecomposeEigen.execute     20       EJML  thrpt   15    10750.193 ±   363.898  ops/s
+DecomposeEigen.execute     20        MTJ  thrpt   15     7868.846 ±   148.955  ops/s
+DecomposeEigen.execute     20     ojAlgo  thrpt   15    17701.635 ±  1400.365  ops/s
+DecomposeEigen.execute     50        ACM  thrpt   15      904.975 ±    38.470  ops/s
+DecomposeEigen.execute     50       EJML  thrpt   15     1195.038 ±    37.892  ops/s
+DecomposeEigen.execute     50        MTJ  thrpt   15     1508.785 ±     9.394  ops/s
+DecomposeEigen.execute     50     ojAlgo  thrpt   15     2124.752 ±    32.394  ops/s
+DecomposeEigen.execute    100        ACM  thrpt   15      130.582 ±     3.751  ops/s
+DecomposeEigen.execute    100       EJML  thrpt   15      209.574 ±     2.295  ops/s
+DecomposeEigen.execute    100        MTJ  thrpt   15      316.339 ±     8.081  ops/s
+DecomposeEigen.execute    100     ojAlgo  thrpt   15      336.345 ±     3.238  ops/s
+DecomposeEigen.execute    200        ACM  thrpt   15       15.718 ±     0.422  ops/s
+DecomposeEigen.execute    200       EJML  thrpt   15       31.971 ±     0.412  ops/s
+DecomposeEigen.execute    200        MTJ  thrpt   15       77.078 ±     1.374  ops/s
+DecomposeEigen.execute    200     ojAlgo  thrpt   15       52.705 ±     0.831  ops/s
+DecomposeEigen.execute    500        ACM  thrpt   15        0.874 ±     0.051  ops/s
+DecomposeEigen.execute    500       EJML  thrpt   15        2.269 ±     0.027  ops/s
+DecomposeEigen.execute    500        MTJ  thrpt   15       10.647 ±     0.098  ops/s
+DecomposeEigen.execute    500     ojAlgo  thrpt   15        3.540 ±     0.037  ops/s
+DecomposeEigen.execute   1000        ACM  thrpt   15        0.054 ±     0.002  ops/s
+DecomposeEigen.execute   1000       EJML  thrpt   15        0.253 ±     0.003  ops/s
+DecomposeEigen.execute   1000        MTJ  thrpt   15        1.976 ±     0.038  ops/s
+DecomposeEigen.execute   1000     ojAlgo  thrpt   15        0.462 ±     0.012  ops/s
  * </pre>
  *
  * MacBook Air: 2015-06-16
- * 
+ *
  * <pre>
  * # Run complete. Total time: 01:41:52
  *
@@ -130,15 +138,9 @@ public class DecomposeEigen extends LinearAlgebraBenchmark {
         LinearAlgebraBenchmark.run(DecomposeEigen.class);
     }
 
-<<<<<<< HEAD
-    @Param({ "2", "3", "4", "5", "10", "20", "50", "100", "200", "500", "1000", "2000"/* , "5000", "10000" */ })
-    public int dim;
-    @Param({ "EJML", "MTJ", "ojAlgo" })
-=======
-    @Param({ "2", "3", "4", "5", "10", "20", "50", "100", "200", "500", "1000", "2000" })
+    @Param({ "2", "3", "4", "5", "10", "20", "50", "100", "200", "500", "1000" })
     public int dim;
     @Param({ "ACM", "EJML", "MTJ", "ojAlgo" })
->>>>>>> branch 'master' of https://github.com/optimatika/ojAlgo-linear-algebra-benchmark.git
     public String library;
 
     Object matrix;
