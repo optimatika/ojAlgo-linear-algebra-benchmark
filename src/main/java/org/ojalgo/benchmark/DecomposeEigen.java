424148a5c18ed1a4bfef6ab9e5a300b6382e8f68
apete <apete@optimatika.se>
2017-04-22 10:07:01 +0200

    Copyright

--- src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
+++ src/main/java/org/ojalgo/benchmark/DecomposeEigen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2015 Optimatika (www.optimatika.se)
+ * Copyright 1997-2017 Optimatika (www.optimatika.se)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,56 +31,56 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.runner.RunnerException;
 
 /**
- * Mac Pro 2017-03-27
+ * Mac Pro 2017-04-22
  *
  * <pre>
-# Run complete. Total time: 00:41:48
+# Run complete. Total time: 00:09:27
 
-Benchmark               (dim)  (library)   Mode  Cnt        Score       Error  Units
-DecomposeEigen.execute      2        ACM  thrpt   15   536795.948 ± 22510.418  ops/s
-DecomposeEigen.execute      2       EJML  thrpt   15  1426954.650 ± 55599.347  ops/s
-DecomposeEigen.execute      2        MTJ  thrpt   15   129182.634 ±  4890.072  ops/s
-DecomposeEigen.execute      2     ojAlgo  thrpt   15  1838395.032 ± 56550.904  ops/s
-DecomposeEigen.execute      3        ACM  thrpt   15   339504.639 ± 17850.133  ops/s
-DecomposeEigen.execute      3       EJML  thrpt   15   564488.161 ± 15962.775  ops/s
-DecomposeEigen.execute      3        MTJ  thrpt   15    57882.905 ±   997.330  ops/s
-DecomposeEigen.execute      3     ojAlgo  thrpt   15   793904.265 ± 39630.311  ops/s
-DecomposeEigen.execute      4        ACM  thrpt   15   233153.670 ±  6469.204  ops/s
-DecomposeEigen.execute      4       EJML  thrpt   15   299188.415 ± 18598.584  ops/s
-DecomposeEigen.execute      4        MTJ  thrpt   15    45431.576 ±   624.977  ops/s
-DecomposeEigen.execute      4     ojAlgo  thrpt   15   501529.762 ± 24517.456  ops/s
-DecomposeEigen.execute      5        ACM  thrpt   15   164323.905 ±  6172.702  ops/s
-DecomposeEigen.execute      5       EJML  thrpt   15   213353.287 ±  7279.367  ops/s
-DecomposeEigen.execute      5        MTJ  thrpt   15    38215.573 ±  1646.812  ops/s
-DecomposeEigen.execute      5     ojAlgo  thrpt   15   314517.883 ± 20166.147  ops/s
-DecomposeEigen.execute     10        ACM  thrpt   15    43903.572 ±  1235.683  ops/s
-DecomposeEigen.execute     10       EJML  thrpt   15    51787.623 ±  2901.456  ops/s
-DecomposeEigen.execute     10        MTJ  thrpt   15    19437.337 ±   500.965  ops/s
-DecomposeEigen.execute     10     ojAlgo  thrpt   15    79890.729 ±  4897.018  ops/s
-DecomposeEigen.execute     20        ACM  thrpt   15     9248.306 ±    73.431  ops/s
-DecomposeEigen.execute     20       EJML  thrpt   15    10750.193 ±   363.898  ops/s
-DecomposeEigen.execute     20        MTJ  thrpt   15     7868.846 ±   148.955  ops/s
-DecomposeEigen.execute     20     ojAlgo  thrpt   15    17701.635 ±  1400.365  ops/s
-DecomposeEigen.execute     50        ACM  thrpt   15      904.975 ±    38.470  ops/s
-DecomposeEigen.execute     50       EJML  thrpt   15     1195.038 ±    37.892  ops/s
-DecomposeEigen.execute     50        MTJ  thrpt   15     1508.785 ±     9.394  ops/s
-DecomposeEigen.execute     50     ojAlgo  thrpt   15     2124.752 ±    32.394  ops/s
-DecomposeEigen.execute    100        ACM  thrpt   15      130.582 ±     3.751  ops/s
-DecomposeEigen.execute    100       EJML  thrpt   15      209.574 ±     2.295  ops/s
-DecomposeEigen.execute    100        MTJ  thrpt   15      316.339 ±     8.081  ops/s
-DecomposeEigen.execute    100     ojAlgo  thrpt   15      336.345 ±     3.238  ops/s
-DecomposeEigen.execute    200        ACM  thrpt   15       15.718 ±     0.422  ops/s
-DecomposeEigen.execute    200       EJML  thrpt   15       31.971 ±     0.412  ops/s
-DecomposeEigen.execute    200        MTJ  thrpt   15       77.078 ±     1.374  ops/s
-DecomposeEigen.execute    200     ojAlgo  thrpt   15       52.705 ±     0.831  ops/s
-DecomposeEigen.execute    500        ACM  thrpt   15        0.874 ±     0.051  ops/s
-DecomposeEigen.execute    500       EJML  thrpt   15        2.269 ±     0.027  ops/s
-DecomposeEigen.execute    500        MTJ  thrpt   15       10.647 ±     0.098  ops/s
-DecomposeEigen.execute    500     ojAlgo  thrpt   15        3.540 ±     0.037  ops/s
-DecomposeEigen.execute   1000        ACM  thrpt   15        0.054 ±     0.002  ops/s
-DecomposeEigen.execute   1000       EJML  thrpt   15        0.253 ±     0.003  ops/s
-DecomposeEigen.execute   1000        MTJ  thrpt   15        1.976 ±     0.038  ops/s
-DecomposeEigen.execute   1000     ojAlgo  thrpt   15        0.462 ±     0.012  ops/s
+Benchmark               (dim)  (library)   Mode  Cnt          Score         Error    Units
+DecomposeEigen.execute      2        ACM  thrpt    3   33397262.344 ±  249323.821  ops/min
+DecomposeEigen.execute      2       EJML  thrpt    3   94220563.635 ± 4838080.227  ops/min
+DecomposeEigen.execute      2        MTJ  thrpt    3    7771431.065 ±   62895.481  ops/min
+DecomposeEigen.execute      2     ojAlgo  thrpt    3  120742245.634 ± 1638043.407  ops/min
+DecomposeEigen.execute      3        ACM  thrpt    3   22625726.902 ± 1820042.504  ops/min
+DecomposeEigen.execute      3       EJML  thrpt    3   40173043.585 ±  515516.541  ops/min
+DecomposeEigen.execute      3        MTJ  thrpt    3    3639615.196 ±  167072.002  ops/min
+DecomposeEigen.execute      3     ojAlgo  thrpt    3   49411207.108 ± 2742733.801  ops/min
+DecomposeEigen.execute      4        ACM  thrpt    3   14315708.901 ±  543309.091  ops/min
+DecomposeEigen.execute      4       EJML  thrpt    3   19613268.425 ±   50017.519  ops/min
+DecomposeEigen.execute      4        MTJ  thrpt    3    3003803.411 ±   14680.159  ops/min
+DecomposeEigen.execute      4     ojAlgo  thrpt    3   29928601.712 ±  514872.775  ops/min
+DecomposeEigen.execute      5        ACM  thrpt    3    9738321.327 ±  186779.450  ops/min
+DecomposeEigen.execute      5       EJML  thrpt    3   13352140.758 ±  271359.713  ops/min
+DecomposeEigen.execute      5        MTJ  thrpt    3    2401550.470 ±  137698.503  ops/min
+DecomposeEigen.execute      5     ojAlgo  thrpt    3   18518925.149 ±  414202.187  ops/min
+DecomposeEigen.execute     10        ACM  thrpt    3    2888860.143 ±   92476.861  ops/min
+DecomposeEigen.execute     10       EJML  thrpt    3    3314815.290 ±   55300.514  ops/min
+DecomposeEigen.execute     10        MTJ  thrpt    3    1212760.102 ±   68935.498  ops/min
+DecomposeEigen.execute     10     ojAlgo  thrpt    3    4534784.144 ±  133178.199  ops/min
+DecomposeEigen.execute     20        ACM  thrpt    3     572653.744 ±   97982.205  ops/min
+DecomposeEigen.execute     20       EJML  thrpt    3     631092.776 ±   39677.499  ops/min
+DecomposeEigen.execute     20        MTJ  thrpt    3     482842.198 ±   19881.610  ops/min
+DecomposeEigen.execute     20     ojAlgo  thrpt    3    1139876.829 ±   29248.264  ops/min
+DecomposeEigen.execute     50        ACM  thrpt    3      52320.760 ±    7918.925  ops/min
+DecomposeEigen.execute     50       EJML  thrpt    3      76655.832 ±    2413.594  ops/min
+DecomposeEigen.execute     50        MTJ  thrpt    3      93036.607 ±    4786.415  ops/min
+DecomposeEigen.execute     50     ojAlgo  thrpt    3     124228.913 ±    2611.090  ops/min
+DecomposeEigen.execute    100        ACM  thrpt    3       8112.929 ±     173.137  ops/min
+DecomposeEigen.execute    100       EJML  thrpt    3      12822.364 ±      90.490  ops/min
+DecomposeEigen.execute    100        MTJ  thrpt    3      19272.925 ±    2311.391  ops/min
+DecomposeEigen.execute    100     ojAlgo  thrpt    3      21422.459 ±     736.925  ops/min
+DecomposeEigen.execute    200        ACM  thrpt    3        942.634 ±      57.384  ops/min
+DecomposeEigen.execute    200       EJML  thrpt    3       1936.929 ±      30.788  ops/min
+DecomposeEigen.execute    200        MTJ  thrpt    3       4473.691 ±     160.030  ops/min
+DecomposeEigen.execute    200     ojAlgo  thrpt    3       3284.895 ±      89.331  ops/min
+DecomposeEigen.execute    500        ACM  thrpt    3         52.764 ±       5.107  ops/min
+DecomposeEigen.execute    500       EJML  thrpt    3        137.048 ±       5.047  ops/min
+DecomposeEigen.execute    500        MTJ  thrpt    3        653.686 ±      56.168  ops/min
+DecomposeEigen.execute    500     ojAlgo  thrpt    3        216.250 ±       1.088  ops/min
+DecomposeEigen.execute   1000        ACM  thrpt    3          3.429 ±       1.446  ops/min
+DecomposeEigen.execute   1000       EJML  thrpt    3         15.457 ±       1.017  ops/min
+DecomposeEigen.execute   1000        MTJ  thrpt    3        122.480 ±      43.279  ops/min
+DecomposeEigen.execute   1000     ojAlgo  thrpt    3         28.640 ±       5.896  ops/min
  * </pre>
  *
  * MacBook Air: 2015-06-16
